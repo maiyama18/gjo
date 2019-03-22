5fe6573da275b621737cb20025f295cf86e25d27
muiscript <myam0118@gmail.com>
2019-03-22 23:50:39 +0900

    complete!

--- encode/encode.go
+++ encode/encode.go
@@ -2,8 +2,13 @@ package encode
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 )
+
+const indent = "  "
 
 func Encode(args []string, array, pretty bool) (string, error) {
 	if array {
@@ -14,15 +19,64 @@ func Encode(args []string, array, pretty bool) (string, error) {
 }
 
 func encodeObject(args []string, pretty bool) (string, error) {
+	keyValues := make(map[string]interface{})
+	for _, arg := range args {
+		key, value, err := parseKeyValue(arg)
+		if err != nil {
+			continue
+		}
+		keyValues[key] = value
+	}
 	buf := &bytes.Buffer{}
-	fmt.Println("object")
-	fmt.Printf("%+v\n", args)
-	return buf.String(), nil
+	enc := json.NewEncoder(buf)
+	if pretty {
+		enc.SetIndent("", indent)
+	}
+
+	err := enc.Encode(keyValues)
+	return buf.String(), err
 }
 
 func encodeArray(args []string, pretty bool) (string, error) {
+	values := make([]interface{}, 0)
+	for _, arg := range args {
+		value, err := parseValue(arg)
+		if err != nil {
+			continue
+		}
+		values = append(values, value)
+	}
 	buf := &bytes.Buffer{}
-	fmt.Println("array")
-	fmt.Printf("%+v\n", args)
-	return buf.String(), nil
+	enc := json.NewEncoder(buf)
+	if pretty {
+		enc.SetIndent("", indent)
+	}
+
+	err := enc.Encode(values)
+	return buf.String(), err
+}
+
+func parseKeyValue(arg string) (string, interface{}, error) {
+	splitted := strings.Split(arg, "=")
+	if len(splitted) != 2 {
+		return "", "", fmt.Errorf("parse error: %s", arg)
+	}
+
+	if val, err := strconv.Atoi(splitted[1]); err == nil {
+		return splitted[0], val, nil
+	}
+	if val, err := strconv.ParseBool(splitted[1]); err == nil {
+		return splitted[0], val, nil
+	}
+	return splitted[0], splitted[1], nil
+}
+
+func parseValue(arg string) (interface{}, error) {
+	if val, err := strconv.Atoi(arg); err == nil {
+		return val, nil
+	}
+	if val, err := strconv.ParseBool(arg); err == nil {
+		return val, nil
+	}
+	return arg, nil
 }
